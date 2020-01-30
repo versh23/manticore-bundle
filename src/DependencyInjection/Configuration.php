1ff2d6526dd2d30d49686fdcad13ac2889c9901b
vershinin_so <vershinin_so@bel-rusnarbank.ru>
2020-01-30 14:24:55 +0300

    add config

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,6 +17,49 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder('manticore');
 
+        $treeBuilder->getRootNode()
+            ->children()
+                ->scalarNode('host')->defaultValue('localhost')->end()
+                ->scalarNode('port')->defaultValue(9306)->end()
+                ->arrayNode('indexes')
+                    ->arrayPrototype()
+                        ->children()
+                            ->scalarNode('class')->end()
+                            ->arrayNode('fields')
+                                ->scalarPrototype()->end()
+                            ->end()
+                            ->arrayNode('attributes')
+                                ->beforeNormalization()
+                                    ->always(function ($attributes) {
+                                        foreach ($attributes as $name => &$attribute) {
+                                            if (is_string($attribute)) {
+                                                $attribute = [
+                                                    'type' => $attribute
+                                                ];
+                                            }
+                                            if (!isset($attribute['property'])) {
+                                                $attribute['property'] = $name;
+                                            }
+                                        }
+                                        return $attributes;
+                                    })
+                                ->end()
+                                ->arrayPrototype()
+                                    ->children()
+                                        ->scalarNode('property')->end()
+                                        ->scalarNode('type')->end()
+                                    ->end()
+                                    ->validate()
+                                        ->ifTrue(function ($v) {return !isset($v['type']);})
+                                        ->thenInvalid('Type is not defined')
+                                    ->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+
         return $treeBuilder;
     }
 }
--- src/DependencyInjection/Versh23ManticoreExtension.php
+++ src/DependencyInjection/Versh23ManticoreExtension.php
@@ -20,6 +20,8 @@ class Versh23ManticoreExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        dd($config);
+
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yaml');
 
